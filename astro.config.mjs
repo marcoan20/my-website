2595bbadaee0cf3e84036e61ad365520e9372a2f
Marco Antônio <marcopierozan20@gmail.com>
2024-03-02 19:38:38 -0300

    :bricks: ci: fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://marcoan20.github.io",
-  base: "/my-website",
   integrations: [tailwind(), react()]
 });
