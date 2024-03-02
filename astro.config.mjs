d37ca6a3bed9653beb69919ded82783e8610b0b6
Marco Antônio <marcopierozan20@gmail.com>
2024-03-02 19:40:24 -0300

    :wrench: fix: revert to last config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://marcoan20.github.io",
+  base: "/my-website",
   integrations: [tailwind(), react()]
 });
